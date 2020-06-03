519f3631a4799edd8c69e59e05bbc37514a7fab7
pthangaraj-board <65935374+pthangaraj-board@users.noreply.github.com>
2020-06-03 10:53:41 +0530

    Create TestCalculator.java

---
+++ src/test/java/TestCalculator.java
@@ -0,0 +1,13 @@
+import com.microsoft.demo.Demo;
+import org.junit.Test;
+
+public class TestCalculator
+{
+  Calculator c = new Calculator();
+  
+  @Test
+  public void testAdd()
+  {
+    assertEquals(5, c.add(2, 3))
+  }
+}
